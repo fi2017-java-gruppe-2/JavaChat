d1f813e7dd94007136a299a2a67701801ba6e06c
derksendaniel <derksendaniel@104WKS07.bsz.edu>
2019-03-29 12:09:12 +0100

    implemented run and ckeckSpam in Spam_Protection

--- src/Spam_Protection.java
+++ src/Spam_Protection.java
@@ -1,5 +1,34 @@
+import java.time.LocalDateTime;
+import java.util.HashMap;
 
-public class Spam_Protection
+public class Spam_Protection implements Runnable
 {
+	private Thread t;
+	private HashMap<String, LocalDateTime> spamliste;
+	private Timer timer;
+	
+	public Spam_Protection()
+	{
+		t = new Thread(this);
+		spamliste = new HashMap<>();
+		t.start();
+	}
+	@Override
+	public void run()
+	{
+		try
+		{
+			Thread.sleep(200);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception in run Spam_Protection");
+		}
+		
+	}
+	private boolean checkSpam(String message, LocalDateTime sendTime, String ip )
+	{
+		return true;	
+	}
 
 }
