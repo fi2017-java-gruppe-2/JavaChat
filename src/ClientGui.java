197b7741d667066bce8ffac04cd83ae7185de714
seuffertmichael <seuffertmichael@104WKS13.bsz.edu>
2019-04-03 14:10:14 +0200

    fix clientgui label

--- src/ClientGui.java
+++ src/ClientGui.java
@@ -41,7 +41,7 @@ public class ClientGui extends JFrame
 	{
 		initialize();
 		c = new ClientControl(labelGesendet, textFieldIP, textFieldPort, textFieldNachricht, textFieldDatei, listTeilnehmer, listNachrichten, listDateien);
-		getContentPane().add(getLabelGesendet());		
+				
 	}
 	
 	public void initialize()
@@ -71,6 +71,7 @@ public class ClientGui extends JFrame
 		getContentPane().add(getTextFieldDatei());
 		getContentPane().add(getLblDatei());
 		getContentPane().add(getListTeilnehmer());
+		getContentPane().add(getLabelGesendet());
 	}
 
 	private JLabel getLblIp()
